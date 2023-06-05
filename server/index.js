49aa047fa43846ea6054290d7073ba2e85de5130
namrata1613 <dasnamrata2402@gmail.com>
2023-06-06 00:52:34 +0530

    routing

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from'cors';
+
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts',postRoutes);
+
+app.use(bodyParser.json({limit:"30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
+app.use(cors());
+
+// mongo
+const CONNECTION_URL = 'mongodb+srv://namratadas:realestateatlas@cluster0.op0vk1l.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology:true})
+.then(() => app.listen(PORT, () => console.log(`Server RUnning : ${PORT}`)))
+.catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
