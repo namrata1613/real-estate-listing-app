efaf33983250f00a00c4e2a4e2edf68a543b0a4a
namrata1613 <dasnamrata2402@gmail.com>
2023-06-06 22:54:18 +0530

    redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 
 import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
-
+import { useDispatch } from 'react-redux';
+import {getPosts} from './actions/posts';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import memories from './images/memories.png';
@@ -10,10 +11,16 @@ import useStyles from './styles';
 
 const App = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    },[dispatch]);
+
     return(
         <Container maxWidth="lg">
             <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+                <Typography className={classes.heading} variant="h2" align="center">EstateConnect</Typography>
                 <img className={classes.image} src={memories} alt="memories" height="60" />
             </AppBar>
             <Grow in>
