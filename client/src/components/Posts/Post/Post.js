59119c084dd513cb1e9d62ecaec1207b54ba5a74
namrata1613 <dasnamrata2402@gmail.com>
2023-06-10 01:44:40 +0530

    deletion

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -4,11 +4,16 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'; 
 import moment from 'moment';
+import { useDispatch } from 'react-redux';
 
 import useStyles from './styles';
+import { deletePost} from "../../../actions/posts";
+
 
 const Post = ({post,setCurrentId}) => {
+    const dispatch = useDispatch();
     const classes = useStyles();
+    
     return (
         <Card classes={classes.card} >
             <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
@@ -39,7 +44,7 @@ const Post = ({post,setCurrentId}) => {
                 <Button size="small" color="primary" onClick={() => {}} > 
                     <ThumbUpAltIcon fontSize="small" /> Like {post.likeCount}
                 </Button>
-                <Button size="small" color="primary" onClick={() => {}} > 
+                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))} > 
                     <DeleteIcon fontSize="small" /> Delete
                 </Button>
             </CardActions>
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,5 @@
-import mongoose from "mongoose";
-import PostMessage from "../models/postMessage.js";
+import mongoose from 'mongoose';
+import PostMessage from '../models/postMessage.js';
 
 export const getPosts = async (req,res) => {
     try {
@@ -27,9 +27,19 @@ export const updatePost = async(req,res) => {
     const {id:_id} = req.params;
     const post = req.body;
 
-    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with this ID');
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with this ID'); // id validation
 
     const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post,_id}, {new:true});
 
     res.json(updatedPost);
+}
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({ message: "Post deleted successfully." });
 }
