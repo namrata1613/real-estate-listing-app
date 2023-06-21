189937964e4f640d03a6439f6b0382a2609e7116
namrata1613 <dasnamrata2402@gmail.com>
2023-06-21 10:30:11 +0530

    refactor

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,13 +1,15 @@
-import { FETCH_ALL,FETCH_BY_SEARCH ,CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+import { FETCH_ALL,FETCH_BY_SEARCH ,CREATE, UPDATE, DELETE, LIKE , START_LOADING,END_LOADING} from '../constants/actionTypes';
 import * as api from '../api';
 
 // action creators
 
 export const getPosts = (page) => async (dispatch) => {
     try {
+        dispatch({type:START_LOADING});
         const {data} = await api.fetchPosts(page);
 
         dispatch({type:FETCH_ALL, payload:data});
+        dispatch({type:END_LOADING});
     } catch (error) {
         console.log(error.message);
     }
@@ -15,9 +17,11 @@ export const getPosts = (page) => async (dispatch) => {
 
 export const getPostsBySearch = (searchQuery) => async (dispatch) => {
     try {
+        dispatch({type:START_LOADING});
         const {data : {data}} = await api.fetchPostsBySearch(searchQuery);
         
         dispatch({type:FETCH_BY_SEARCH, payload:data});
+        dispatch({type:END_LOADING});
     } catch (error) {
         console.log(error.message);
     }
@@ -25,9 +29,11 @@ export const getPostsBySearch = (searchQuery) => async (dispatch) => {
 
 export const createPost = (post) => async (dispatch) => {
     try {
+        dispatch({type:START_LOADING});
         const {data} = await api.createPost(post);
 
         dispatch({type:CREATE, payload:data});
+        dispatch({type:END_LOADING});
     } catch (error) {
         console.log(error);
     }
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -5,12 +5,12 @@ import Post from "./Post/Post";
 import useStyles from './styles';
 
 const Posts = ({setCurrentId}) => {
-    const {posts} = useSelector((state) => state.posts);
+    const {posts, isLoading} = useSelector((state) => state.posts);
     const classes = useStyles();
 
-    console.log(posts);
+    if(!posts.length && !isLoading) return 'No Posts';
     return (
-        !posts?.length ? <CircularProgress /> : (
+        isLoading ? <CircularProgress /> : (
             <Grid className={classes.container} container alignItems="stretch" spacing={3}>
                 {posts.map((post) => (
                     <Grid key={post._id} item xs={12} sm={6} lg={6}>   
