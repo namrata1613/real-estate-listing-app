9a74f978efb98f4509214c0f7bc0e2f7956c2c7e
namrata1613 <dasnamrata2402@gmail.com>
2023-06-23 14:07:20 +0530

    refactor

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Card, CardActions, CardContent, CardMedia, Button, Typography, ButtonBase} from '@material-ui/core/';
+import {Card, CardActions, CardContent, CardMedia, Button, Typography} from '@material-ui/core/';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import ThumbUpAltOutlined from '@material-ui/icons/ThumbUpAltOutlined';
 import DeleteIcon from '@material-ui/icons/Delete';
@@ -7,7 +7,7 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import { useDispatch } from 'react-redux';
 import {useHistory} from 'react-router-dom';
-
+import { useState } from "react";
 import useStyles from './styles';
 import { deletePost,likePost} from "../../../actions/posts";
 
@@ -17,14 +17,28 @@ const Post = ({post,setCurrentId}) => {
     const classes = useStyles();
     const history = useHistory();
     const user = JSON.parse(localStorage.getItem('profile'));
+    const [likes,setLikes] = useState(post?.likes);
+    const userId = (user?.result?.googleId || user?.result?._id);
+    const hasLikedPost = post.likes.find((like) => like === userId);
+    
+
+    const handleClick = async () => {
+        dispatch(likePost(post._id));
+        
+        if(hasLikedPost) {
+            setLikes(post.likes.filter((id) => id !== userId));
+        }else{
+            setLikes([... post.likes,userId]);
+        }
+    };
 
     const Likes = () => {
-        if(post.likes.length > 0){
-            return post.likes.find((like) => like === (user?.result?.googleId || user?.result?._id)) ? 
+        if(likes.length > 0){
+            return likes.find((like) => like === userId) ? 
             (
-                <><ThumbUpAltIcon fontSize="small" />&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length -1} others` : `${post.likes.length} like${post.likes.length >1 ? 's':''}`}</>
+                <><ThumbUpAltIcon fontSize="small" />&nbsp;{likes.length > 2 ? `You and ${likes.length -1} others` : `${likes.length} like${likes.length >1 ? 's':''}`}</>
             ) : (
-                <><ThumbUpAltOutlined fontSize="small" />&nbsp;{post.likes.length} {post.likes.length ===1 ? 'Like':'Likes'}</>
+                <><ThumbUpAltOutlined fontSize="small" />&nbsp;{likes.length} {likes.length ===1 ? 'Like':'Likes'}</>
             );
         }
         return <><ThumbUpAltOutlined fontSize="small" />&nbsp;Like</>;
@@ -63,7 +77,7 @@ const Post = ({post,setCurrentId}) => {
             
 
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" disabled={!user?.result} onClick={() => dispatch(likePost(post._id))} > 
+                <Button size="small" color="primary" disabled={!user?.result} onClick={handleClick} > 
                     <Likes/>
                 </Button>
                 {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
